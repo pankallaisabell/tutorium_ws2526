cf874182a1787b92e1d447df50bc250e795b67f0
Isabell <ipankalla@hof-university.de>
2025-11-20 10:40:44 +0100

    some more comments

--- src/t201125/PlaetzchenArt.java
+++ src/t201125/PlaetzchenArt.java
@@ -2,13 +2,13 @@ package t201125;
 
 public enum PlaetzchenArt {
 
-    VANILLEKIPFERL("Vanillekipferl auf die 1"),
+    VANILLEKIPFERL("Vanillekipferl auf die 1"), // Komma-getrennt
     ZIMTSTERN("würzig und gut"),
-    BUTTERPLAETZCHEN("ein einfacher, süßer Klassiker");
+    BUTTERPLAETZCHEN("ein einfacher, süßer Klassiker"); // Semikolon am Ende
 
     private String beschreibung;
 
-    private PlaetzchenArt(String beschreibung) {
+    private PlaetzchenArt(String beschreibung) { // wichtig: private
         this.beschreibung = beschreibung;
     }
 
