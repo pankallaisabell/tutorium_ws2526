78c61fd9879130ab5c0f13f5cc6060d61e18300a
Isabell <ipankalla@hof-university.de>
2025-11-27 10:38:22 +0100

    Material 27. Nov.

--- src/t271125/Geschenk.java
+++ src/t271125/Geschenk.java
@@ -1,4 +1,19 @@
 package t271125;
 
 public class Geschenk {
+    private String name;
+    private int gewicht; // in Gramm
+
+    public Geschenk(String name, int gewicht) {
+        this.name = name;
+        this.gewicht = gewicht;
+    }
+
+    public String getName() { return name; }
+    public int getGewicht() { return gewicht; }
+
+    @Override
+    public String toString() {
+        return name + " (" + gewicht + "g)";
+    }
 }
--- src/t271125/Palette.java
+++ src/t271125/Palette.java
@@ -1,4 +1,14 @@
 package t271125;
 
 public class Palette {
+    private String id;
+    private Geschenk geschenk;
+
+    public Palette(String id, Geschenk geschenk) {
+        this.id = id;
+        this.geschenk = geschenk;
+    }
+
+    public String getId() { return id; }
+    public Geschenk getGeschenk() { return geschenk; }
 }
